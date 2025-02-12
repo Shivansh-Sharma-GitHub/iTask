bb902aba560f2b051e52602cee74d06796894884
Ved Tusharbhai Patel <131395790+Ved609@users.noreply.github.com>
2025-02-12 17:34:30 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default [
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
+      'react-hooks': reactHoo,
       'react-refresh': reactRefresh,
     },
     rules: {
